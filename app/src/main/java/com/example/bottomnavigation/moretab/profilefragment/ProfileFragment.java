6aa3d43a0d15b584e3ec5bf1e12d67162fb9c42e
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-08-06 19:26:40 +0430

    Set PersianDatePicker

--- app/src/main/java/com/example/bottomnavigation/moretab/profilefragment/ProfileFragment.java
+++ app/src/main/java/com/example/bottomnavigation/moretab/profilefragment/ProfileFragment.java
@@ -1,11 +1,15 @@
 package com.example.bottomnavigation.moretab.profilefragment;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,6 +20,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -27,18 +32,24 @@ import com.example.bottomnavigation.data.model.User;
 import com.example.bottomnavigation.data.repository.IsLoginRepository;
 import com.example.bottomnavigation.login.di.LoginModule;
 import com.example.bottomnavigation.moretab.di.MoreModule;
+import ir.hamsaa.persiandatepicker.Listener;
+import ir.hamsaa.persiandatepicker.PersianDatePickerDialog;
+import ir.hamsaa.persiandatepicker.util.PersianCalendar;
 
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
 
     private RadioGroup radioSexGroup;
-    private RadioButton radioSexButton, male, female;
+    private RadioButton radioSexButton;
     private EditText name, date;
     private ProfileViewModel profileViewModel;
     private UserDataSource userLocalDataSource = LoginModule.provideUserDataSource();
     private IsLoginRepository isLoginRepository = LoginModule.provideIsLoginRepository(userLocalDataSource);
     private ProfileViewModelFactory profileViewModelFactory = MoreModule.provideProfileViewModelFactory(isLoginRepository);
+    private PersianDatePickerDialog picker;
+
 
     @Nullable
     @Override
@@ -50,6 +61,7 @@ public class ProfileFragment extends Fragment {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -65,8 +77,8 @@ public class ProfileFragment extends Fragment {
         date = view.findViewById(R.id.date);
         Button change = view.findViewById(R.id.change);
         Button cancel = view.findViewById(R.id.cancle);
-        male = view.findViewById(R.id.male);
-        female = view.findViewById(R.id.female);
+        RadioButton male = view.findViewById(R.id.male);
+        RadioButton female = view.findViewById(R.id.female);
 
         assert userEntity != null;
 
@@ -78,13 +90,21 @@ public class ProfileFragment extends Fragment {
         name.setText(userEntity.getName());
         date.setText(userEntity.getDate());
 
-        String checkedGender= userEntity.getGender();
-        if (checkedGender.equals("مرد")){
+        String checkedGender = userEntity.getGender();
+        if (checkedGender.equals("مرد")) {
             male.setChecked(true);
-        }
-        else if (checkedGender.equals("زن")){
+        } else if (checkedGender.equals("زن")) {
             female.setChecked(true);
         }
+
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCalendar();
+            }
+        });
+
 
         change.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,8 +128,6 @@ public class ProfileFragment extends Fragment {
                 cancelAsyncTask.execute();
             }
         });
-
-
     }
 
     public void addListenerOnButton(final View view) {
@@ -122,5 +140,46 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public void showCalendar() {
+
+        PersianCalendar initDate = new PersianCalendar();
+        initDate.setPersianDate(1370, 3, 13);
+        picker = new PersianDatePickerDialog(getContext())
+                .setPositiveButtonString("باشه")
+                .setNegativeButton("بیخیال")
+                .setTodayButton("امروز")
+                .setTodayButtonVisible(true)
+                .setMinYear(1300)
+                .setActionTextColor(R.color.white)
+                .setMaxYear(PersianDatePickerDialog.THIS_YEAR)
+                .setActionTextColor(Color.GRAY)
+                .setTitleType(PersianDatePickerDialog.WEEKDAY_DAY_MONTH_YEAR)
+                .setShowInBottomSheet(true)
+                .setListener(new Listener() {
+                    @Override
+                    public void onDateSelected(PersianCalendar persianCalendar) {
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.getGregorianChange());//Fri Oct 15 03:25:44 GMT+04:30 1582
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.getTimeInMillis());//1583253636577
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.getTime());//Tue Mar 03 20:10:36 GMT+03:30 2020
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.getDelimiter());//  /
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.getPersianLongDate());// سه‌شنبه  13  اسفند  1398
+//                        Log.d(TAG, "onDateSelected: " + persianCalendar.getPersianLongDateAndTime()); //سه‌شنبه  13  اسفند  1398 ساعت 20:10:36
+//                        Log.d(TAG, "onDateSelected: " + persianCalendar.getPersianMonthName()); //اسفند
+                        Log.d(TAG, "onDateSelected: " + persianCalendar.isPersianLeapYear());//false
+
+                        date.setText(persianCalendar.getPersianShortDate());
+                    }
+
+                    @Override
+                    public void onDismissed() {
+
+                    }
+
+                });
+
+        picker.show();
     }
 }
