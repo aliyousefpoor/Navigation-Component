5f15c0117995106018116e07c2c5d4c355517e3d
Ali Yousefpoor <Ali@Mahdis-MBP.lan>
2020-09-06 14:17:17 +0430

    add getProductComment in RemoteDataSource

--- app/src/main/java/com/example/bottomnavigation/data/datasource/remote/ProductDetailRemoteDataSource.java
+++ app/src/main/java/com/example/bottomnavigation/data/datasource/remote/ProductDetailRemoteDataSource.java
@@ -2,9 +2,12 @@ package com.example.bottomnavigation.data.datasource.remote;
 
 import com.example.bottomnavigation.ApiService;
 import com.example.bottomnavigation.data.datasource.DataSourceListener;
+import com.example.bottomnavigation.data.model.Comment;
 import com.example.bottomnavigation.data.model.ProductsList;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,6 +29,20 @@ public class ProductDetailRemoteDataSource {
 
             @Override
             public void onFailure(@NotNull Call<ProductsList> call, @NotNull Throwable t) {
+                dataSourceListener.onFailure(t);
+            }
+        });
+    }
+
+    public void getProductComment(int id, final DataSourceListener<List<Comment>> dataSourceListener) {
+        apiService.getComment(id).enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Comment>> call, @NotNull Response<List<Comment>> response) {
+                dataSourceListener.onResponse(response.body());
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Comment>> call, @NotNull Throwable t) {
                 dataSourceListener.onFailure(t);
             }
         });
